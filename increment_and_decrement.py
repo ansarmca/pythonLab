1649c7f14167865a370956782cccc7d52bb78c10
ansarmca <ansarkp300@gmail.com>
2024-10-01 18:51:28 +0530

    Update increment_and_decrement.py

--- increment_and_decrement.py
+++ increment_and_decrement.py
@@ -2,7 +2,7 @@
 number = int(input("Enter a number: "))
 
 incremented_number = number + 5
-print("Number after incrementing by 5: " ,incremented_number)
+print("Number after incrementing by 5: :",incremented_number)
 
-decremented_number = number - 3
-print("Number after decrementing by 3: ", decremented_number)
+decremented_number = incremented_number - 3
+print("Number after decrementing by 3: ",decremented_number)
