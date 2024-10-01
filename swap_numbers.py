c3bbb5dc19aa2eeaf011ae5f3b9da7e2f3fb5fc1
ansarmca <ansarkp300@gmail.com>
2024-10-01 12:12:05 +0530

    Add files via upload

---
+++ swap_numbers.py
@@ -0,0 +1,10 @@
+x = int(input("Enter first Number: "))
+y = int(input("Enter second Number: "))
+
+print ("Before swapping: ")
+print("Value of x : ", x, " and y : ", y)
+
+x, y = y, x
+
+print ("After swapping: ")
+print("Value of x : ", x, " and y : ", y)
