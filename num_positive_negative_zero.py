b08c67002c373da2c3350965f5e35e1c895643a9
ansarmca <ansarkp300@gmail.com>
2024-10-04 11:17:16 +0530

    Update num_positive_negative_zero.py

--- num_positive_negative_zero.py
+++ num_positive_negative_zero.py
@@ -1,7 +1,8 @@
 num=int(input("Enter Number: "))
 if (num>=0):
-    print("Number is Positive")
     if (num==0):
         print("Number is Zero")
+    else:
+        print("Number is Positive")
 else:
     print("Number is Negative")
