620eec030dcc6316d95e601ae3f3d1549d97031f
ansarmca <ansarkp300@gmail.com>
2024-10-09 10:50:33 +0530

    Update vowels.py

--- Octobar/week 2/vowels.py
+++ Octobar/week 2/vowels.py
@@ -1,8 +1,8 @@
-str="ansar"
-vowels = ['a','e','i','o','u']
+str="Ansar"
+vowels = ['a','e','i','o','u','A','E','I','O','O']
 vowel_count=0
 
-for char in str.lower():
+for char in str:
     if char in vowels:
         vowel_count+=1
 print("number of vowels in the string: ",vowel_count)
