363fbd06ec8f0399c23f15341a208b7219d10380
ansarmca <ansarkp300@gmail.com>
2024-10-09 10:59:08 +0530

    Add files via upload

---
+++ Octobar/week 2/swap_first_and_last_element.py
@@ -0,0 +1,13 @@
+a=[]
+n= int(input("Enter the number of elements in list:"))
+
+for x in range(0,n):
+    element=int(input("Enter element" + str(x+1) + ":"))
+    a.append(element)
+print("Before swapping: ",a)
+
+temp=a[0]
+a[0]=a[n-1]
+a[n-1]=temp
+
+print("After swapping :",a)
