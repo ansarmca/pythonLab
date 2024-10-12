f630a0d02cd07c2dd3bd1ddc371b21dfc7866b5a
ansarmca <ansarkp300@gmail.com>
2024-10-12 11:03:05 +0530

    Delete increment_and_decrement.py

--- increment_and_decrement.py
+++
@@ -1,8 +0,0 @@
-# Increment the number by 5 and then decrement the number by 3
-number = int(input("Enter a number: "))
-
-incremented_number = number + 5
-print("Number after incrementing by 5: :",incremented_number)
-
-decremented_number = incremented_number - 3
-print("Number after decrementing by 3: ",decremented_number)
