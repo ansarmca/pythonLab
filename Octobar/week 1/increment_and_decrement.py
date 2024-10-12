e419d69aa9a512628fe11089e71ec1bacb32dec5
ansarmca <ansarkp300@gmail.com>
2024-10-12 11:01:18 +0530

    Create increment_and_decrement.py

---
+++ Octobar/week 1/increment_and_decrement.py
@@ -0,0 +1,15 @@
+# Increment the number by 5 and then decrement the number by 3
+
+number = int(input("Enter a number: "))
+
+
+
+incremented_number = number + 5
+
+print("Number after incrementing by 5: :",incremented_number)
+
+
+
+decremented_number = incremented_number - 3
+
+print("Number after decrementing by 3: ",decremented_number)
