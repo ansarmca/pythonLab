d17509ed525ee50c32a7a691dbd235099fdc111e
ansarmca <ansarkp300@gmail.com>
2024-10-16 16:26:20 +0530

    Update add_elements_to_list.py

--- Octobar/week3/add_elements_to_list.py
+++ Octobar/week3/add_elements_to_list.py
@@ -1,13 +1,9 @@
-# Create an empty list
 user_list = []
 
-# Get the number of elements from the user
 num_elements = int(input("How many elements do you want to add? "))
 
-# Use a loop to input each element
 for i in range(num_elements):
-    element = input(f"Enter element {i+1}: ")
+    element = input("Enter element :" )
     user_list.append(element)
 
-# Print the final list
 print("The list is:", user_list)
