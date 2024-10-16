3b1ef28b5a0ab7420d9132733da6bd853ac24963
ansarmca <ansarkp300@gmail.com>
2024-10-16 16:42:43 +0530

    Update occurance_of_a_in_list.py

--- Octobar/week3/occurance_of_a_in_list.py
+++ Octobar/week3/occurance_of_a_in_list.py
@@ -1,12 +1,8 @@
-# Input a list of first names
-first_names = ["Alice", "Anna", "Jack", "Amanda", "Sarah"]
+first_names = ["Alice", "Anas", "Raheem", "fazil"]
 
-# Initialize a counter for occurrences of 'a'
 a_count = 0
 
-# Iterate through each name and count 'a'
 for name in first_names:
-    a_count += name.lower().count('a')  # Use lower() to count both 'a' and 'A'
+    a_count += name.lower().count('a')
 
-# Print the total count of 'a'
 print("The letter 'a' occurs", a_count, "times in the list.")
