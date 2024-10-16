2e67197673628026260390a26a758ef805ba5686
ansarmca <ansarkp300@gmail.com>
2024-10-16 16:32:06 +0530

    Update count_occurance_of_word.py

--- Octobar/week3/count_occurance_of_word.py
+++ Octobar/week3/count_occurance_of_word.py
@@ -1,19 +1,19 @@
-# Input line of text
+
 text = input("Enter a line of text: ")
 
-# Split the text into words
+
 words = text.split()
 
-# Create a dictionary to store word counts
+
 word_count = {}
 
-# Count occurrences of each word
+
 for word in words:
     if word in word_count:
         word_count[word] += 1
     else:
         word_count[word] = 1
 
-# Print the word counts
+
 for word, count in word_count.items():
     print("'" + word + "' occurs " + str(count) + " times.")
