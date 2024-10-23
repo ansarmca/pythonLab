73fc397beb5ddd9f641cf9e81b50d6dfbe2e28bd
ansarmca <ansarkp300@gmail.com>
2024-10-23 15:41:04 +0530

    Add files via upload

---
+++ Octobar/week 4/fibonacci_series.py
@@ -0,0 +1,16 @@
+n = int(input("Enter the number of terms: "))
+
+if n <= 0:
+    print("Please enter a positive integer.")
+else:
+    first = 0
+    second = 1
+    print("Fibonacci Series:")
+    for i in range(n):
+        if i <= 1:
+            result = i
+        else:
+            result = first + second
+            first = second
+            second = result
+        print(result)
