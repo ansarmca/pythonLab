20d4df9ff85a369cd41d435e5ecdd221c8fa1e78
ansarmca <ansarkp300@gmail.com>
2024-11-06 15:27:30 +0530

    Update menu_driven_program.py

--- November/Week 1/menu_driven_program.py
+++ November/Week 1/menu_driven_program.py
@@ -1,42 +1,40 @@
-def find_greatest_and_lowest(numbers):
-    print("Greatest number:", max(numbers))
-    print("Lowest number:", min(numbers))
+def calculate_grade(total_marks):
+    if total_marks >= 90:
+        return 'A'
+    elif total_marks >= 82:
+        return 'B'
+    elif total_marks >= 75:
+        return 'C'
+    elif total_marks >= 60:
+        return 'D'
+    elif total_marks >= 50:
+        return 'P'
+    else:
+        return 'F'
 
-def sort_ascending(numbers):
-    numbers.sort()
-    print("Sorted list:", numbers)
+name = input("Enter student's name: ")
+roll_number = int(input("Enter roll number: "))
+register_number = input("Enter register number: ")
+department = input("Enter department: ")
+semester = int(input("Enter semester: "))
 
-def create_even_list(numbers):
-    even_numbers = []
-    for num in numbers:
-        if num % 2 == 0:
-            even_numbers.append(num)
-    print("Even numbers:", even_numbers)
+student_details = {
+    'name': name,
+    'roll_number': roll_number,
+    'register_number': register_number,
+    'department': department,
+    'semester': semester,
+}
+print("Entered student details:", student_details)
 
-def main():
-    n = int(input("Enter the number of elements: "))
-    numbers = []
-    for i in range(n):
-        num = int(input("Enter number {}: ".format(i+1)))
-        numbers.append(num)
+total_marks = int(input("Enter total marks: "))
+student_details['total_marks'] = total_marks
 
-    while True:
-        print("\nMenu:")
-        print("1. Find greatest and lowest numbers")
-        print("2. Sort list in ascending order")
-        print("3. Create a list of even numbers")
-        print("4. Exit")
-        choice = int(input("Enter your choice: "))
+grade = calculate_grade(total_marks)
+student_details['grade'] = grade
 
-        if choice == 1:
-            find_greatest_and_lowest(numbers)
-        elif choice == 2:
-            sort_ascending(numbers)
-        elif choice == 3:
-            create_even_list(numbers)
-        elif choice == 4:
-            break
-        else:
-            print("Invalid choice. Please try again.")
+print("\nUpdated student details : ",student_details)
 
-main()
+del student_details['roll_number']
+
+print("\nStudent details after rollno deletion: ",student_details)
