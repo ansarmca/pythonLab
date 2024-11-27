eb4119acf7ff5a7c89cc9fd8c2c0839b35edce84
ansarmca <ansarkp300@gmail.com>
2024-11-27 11:49:26 +0530

    Create area.py

---
+++ November/week 5/sub_packages/math_oprations/basic_operations/geomatric_operations/area.py
@@ -0,0 +1,7 @@
+def rect_area(length,width):
+        return length*width
+def circle_area(radius):
+        from math import pi
+        return radius*radius
+def triangle_area(base,height):
+        return base*height        
