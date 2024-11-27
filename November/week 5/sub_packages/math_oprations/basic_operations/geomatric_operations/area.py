ac0b795dcb4187bf850f1e96b00f760959c2604d
ansarmca <ansarkp300@gmail.com>
2024-11-27 11:54:26 +0530

    Delete November/week 5/sub_packages/math_oprations/basic_operations/geomatric_operations directory

--- November/week 5/sub_packages/math_oprations/basic_operations/geomatric_operations/area.py
+++
@@ -1,7 +0,0 @@
-def rect_area(length,width):
-        return length*width
-def circle_area(radius):
-        from math import pi
-        return radius*radius
-def triangle_area(base,height):
-        return base*height        
